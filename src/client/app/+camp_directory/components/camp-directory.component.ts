f8f9943f41719a01e2ae942e20b8502dbe3ed8fc
Eric Zhang <eric.zhang@frontdeskhq.com>
2016-04-27 13:50:34 -0700

    implement UI of directory

--- src/client/app/+camp_directory/components/camp-directory.component.ts
+++ src/client/app/+camp_directory/components/camp-directory.component.ts
@@ -1,9 +1,19 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+import {CampService, Camp} from '../../shared/index';
 
 @Component({
 	selector: 'p1-camp-directory',
   templateUrl: 'app/+camp_directory/components/camp-directory.component.html',
-  styleUrls: ['app/+camp_directory/components/camp-directory.component.css']
+  styleUrls: ['app/+camp_directory/components/camp-directory.component.css'],
+  providers: [CampService]
 })
 
-export class CampDirectoryComponent {}
+export class CampDirectoryComponent {
+	selectedCategory = 'home';
+	categories = ['home','friends','family','photos'];
+
+	clickCategory(category: string) {
+		this.selectedCategory = category;
+	}
+}
